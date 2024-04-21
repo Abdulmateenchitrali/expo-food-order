e94a6496fda75853050954f27c7fb4e05362b2c5
abdulmateen <abdulmateen@sleepwellsystem.com>
2024-04-21 14:32:16 +0500

    create first compoonent

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,37 +4,13 @@ import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import Colors from '@/constants/Colors';
 import products from '@/assets/data/products';
+import ProductListItem from '@/components/ProductListItem';
 
-export default function TabOneScreen() {
-
-  const product = products[0];
-
+export default function MenuScreen() {
+  
   return (
-    <View style={styles.container}>
-    <Image  source={{uri:product.image}} style={styles.image} />
-     <Text style={styles.title}>{product.name}</Text>
-     <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product={products[0]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor:'white',
-    padding:10,
-    borderRadius:20,
-  },
-  title: {
-   fontSize:20,
-   fontWeight: 'bold',
-   marginVertical:10
-  },
-  price:{
-    color:Colors.light.tint
-  },
-  image:{
-    width:"100%",
-    aspectRatio:1,
-  }
-  
-});
